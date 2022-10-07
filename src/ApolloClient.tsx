e205ea05f244cfa3c98c5ba6b9a1e9aecadd650a
Thomas <thomasrnunez@gmail.com>
2022-10-07 00:40:25 -0400

    deployed to azure

--- src/ApolloClient.tsx
+++ src/ApolloClient.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:5021/graphql',
+    uri: 'https://lemonadestand.happyfield-a4099696.eastus.azurecontainerapps.io/graphql',
     cache: new InMemoryCache(),
     headers: {
         "Access-Control-Allow-Origin": "*",
