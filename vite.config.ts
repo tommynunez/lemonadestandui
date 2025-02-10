50ecea35dc733329d119017ef4e7b45389adeb81
Thomas <thomasrnunez@gmail.com>
2025-02-10 12:27:27 -0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-	plugins: [macrosPlugin(), reactPlugin(), plugin(), nodeResolve(), commonjs],
+	plugins: [macrosPlugin(), reactPlugin(), plugin(), nodeResolve(), commonjs()],
 	build: {
 		rollupOptions: {
 			onwarn(warning, warn) {
