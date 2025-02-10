44eddb59c4242be86218b865238f1af1078e7dcd
Thomas <thomasrnunez@gmail.com>
2025-02-10 00:46:52 -0500

    wip

--- src/components/lemonadestand/index.tsx
+++ src/components/lemonadestand/index.tsx
@@ -18,313 +18,321 @@ import { useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import { TFormFields } from '../../types/TFormFields';
 
-const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-const phoneNumberRegex = /^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/i;
+const emailRegex =
+	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const phoneNumberRegex =
+	/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/i;
 
 const formsInitialstate: Array<TForm> = [
-    {
-        name: "product",
-        formHasLoaded: false,
-        formFields: []
-    },
-    {
-        name: "contact",
-        formHasLoaded: false,
-        formFields: [
-            {
-                formAttribute:
-                {
-                    id: "firstName",
-                    name: "firstName",
-                    label: "First Name",
-                },
-                isTouched: false,
-
-            },
-            {
-                formAttribute:
-                {
-                    id: "lastName",
-                    name: "lastName",
-                    label: "Last Name",
-                },
-                isTouched: false,
-
-            },
-            {
-                formAttribute:
-                {
-                    id: "email",
-                    name: "email",
-                    label: "Email",
-                },
-                isTouched: false,
-
-            },
-            {
-                formAttribute:
-                {
-                    id: "phone",
-                    name: "phone",
-                    label: "Phone",
-                },
-                isTouched: false,
-
-            },
-        ]
-    },
+	{
+		name: 'product',
+		formHasLoaded: false,
+		formFields: [],
+	},
+	{
+		name: 'contact',
+		formHasLoaded: false,
+		formFields: [
+			{
+				formAttribute: {
+					id: 'firstName',
+					name: 'firstName',
+					label: 'First Name',
+				},
+				isTouched: false,
+			},
+			{
+				formAttribute: {
+					id: 'lastName',
+					name: 'lastName',
+					label: 'Last Name',
+				},
+				isTouched: false,
+			},
+			{
+				formAttribute: {
+					id: 'email',
+					name: 'email',
+					label: 'Email',
+				},
+				isTouched: false,
+			},
+			{
+				formAttribute: {
+					id: 'phone',
+					name: 'phone',
+					label: 'Phone',
+				},
+				isTouched: false,
+			},
+		],
+	},
 ];
 
 const formhandlerIninitialState: FormHandler = {
-    isDirty: false,
-    forms: formsInitialstate,
+	isDirty: false,
+	forms: formsInitialstate,
 };
 
 const contactInformationIninitialState: ContactInformation = {
-    firstName: undefined,
-    lastName: undefined,
-    email: undefined,
-    phone: undefined,
+	firstName: undefined,
+	lastName: undefined,
+	email: undefined,
+	phone: undefined,
 };
 
 const LemonadeStand = () => {
-    //modal state
-    const [open, setOpen] = React.useState(false);
-    //stepper state
-    const [activeStep, setActiveStep] = React.useState<number>(0);
-    const steps = ['Place Order', 'Customer Information', 'Review Order'];
-    //product step state
-    const [lineItems, setLineItem] = useState<Array<LineItem>>([]);
-    //contact step state
-    const [contactInformation, setcontactInformation] = useState<ContactInformation>(contactInformationIninitialState);
+	//modal state
+	const [open, setOpen] = React.useState(false);
+	//stepper state
+	const [activeStep, setActiveStep] = React.useState<number>(0);
+	const steps = ['Place Order', 'Customer Information', 'Review Order'];
+	//product step state
+	const [lineItems, setLineItem] = useState<Array<LineItem>>([]);
+	//contact step state
+	const [contactInformation, setcontactInformation] =
+		useState<ContactInformation>(contactInformationIninitialState);
 
-    //Form handler state
-    const [error, setErrors] = useState<ErrorHandler>({} as ErrorHandler);
-    const [formhandler, setFormHandler] = useState<FormHandler>(formhandlerIninitialState);
+	//Form handler state
+	const [error, setErrors] = useState<ErrorHandler>({} as ErrorHandler);
+	const [formhandler, setFormHandler] = useState<FormHandler>(
+		formhandlerIninitialState
+	);
 
-    const [addOrder, { loading }] = useMutation(ADD_ORDER);
-    const navigate = useNavigate();
+	const [addOrder, { loading }] = useMutation(ADD_ORDER);
+	const navigate = useNavigate();
 
-    const setFormhasLoadedTrue = (formhandler: any, formIndex: number) => {
-        formhandler.forms[formIndex].formHasLoaded = true;
-        setFormHandler(formhandler);
-    };
+	const setFormhasLoadedTrue = (formhandler: any, formIndex: number) => {
+		formhandler.forms[formIndex].formHasLoaded = true;
+		setFormHandler(formhandler);
+	};
 
-    const setFormhasLoadedFalse = (formhandler: any, formIndex: number) => {
-        formhandler.forms[formIndex].formHasLoaded = false;
-        setFormHandler(formhandler);
-    };
+	const setFormhasLoadedFalse = (formhandler: any, formIndex: number) => {
+		formhandler.forms[formIndex].formHasLoaded = false;
+		setFormHandler(formhandler);
+	};
 
-    const handleClose = () => {
-        setOpen(false);
-        handleForcingIsTouchedonallFields(true);
-    };
+	const handleClose = () => {
+		setOpen(false);
+		handleForcingIsTouchedonallFields(true);
+	};
 
-    const handleSettingFormhandlerFields = (formhandler: FormHandler, setFormHandler: any, activeStep: number, index: number) => {
-        formhandler.isDirty = true;
-        let form = formhandler?.forms[activeStep];
-        let fields = form?.formFields;
-        fields[index].isTouched = true;
-        form.formFields = fields;
-        setFormHandler(formhandler);
-    }
+	const handleSettingFormhandlerFields = (
+		formhandler: FormHandler,
+		setFormHandler: any,
+		activeStep: number,
+		index: number
+	) => {
+		formhandler.isDirty = true;
+		let form = formhandler?.forms[activeStep];
+		let fields = form?.formFields;
+		fields[index].isTouched = true;
+		form.formFields = fields;
+		setFormHandler(formhandler);
+	};
 
-    const handleForcingIsTouchedonallFields = (value: boolean) => {
-        let formFields = [] as Array<TFormFields>;
-        formhandler.forms[activeStep].formFields.map((item: TFormFields, index) => {
-            if (activeStep === 0 && (!lineItems[index]?.quantity || lineItems[index]?.quantity === 0)) {
-                item.isTouched = value;
-                formFields.push(item);
-            }
+	const handleForcingIsTouchedonallFields = (value: boolean) => {
+		let formFields = [] as Array<TFormFields>;
+		formhandler.forms[activeStep].formFields.map((item: TFormFields, index) => {
+			if (
+				activeStep === 0 &&
+				(!lineItems[index]?.quantity || lineItems[index]?.quantity === 0)
+			) {
+				item.isTouched = value;
+				formFields.push(item);
+			}
 
-            if (activeStep === 1 && !contactInformation[index]) {
-                item.isTouched = value;
-                formFields.push(item);
-            }
-        });
+			if (activeStep === 1 && !contactInformation[index]) {
+				item.isTouched = value;
+				formFields.push(item);
+			}
+		});
 
-        const productForm = { ...formhandler };
-        productForm.forms[activeStep].formFields = formFields;
-        setFormHandler(productForm);
-    }
+		const productForm = { ...formhandler };
+		productForm.forms[activeStep].formFields = formFields;
+		setFormHandler(productForm);
+	};
 
-    const handleCheckinglineItem = () => {
-        return lineItems.filter((item, index) => {
-            return item.quantity > 0;
-        }).length === 0;
-    }
+	const handleCheckinglineItem = () => {
+		return (
+			lineItems.filter((item, index) => {
+				return item.quantity > 0;
+			}).length === 0
+		);
+	};
 
-    const handleValidatingcontactInformation = () => {
-        const contactInformationkeys = Object.keys(contactInformation as ContactInformation);
-        return formhandler?.forms[activeStep]?.formFields?.filter((item, index) => {
-            return (!contactInformation[contactInformationkeys[index]]
-                || (contactInformationkeys[index] === "email"
-                    && !emailRegex.test(contactInformation[contactInformationkeys[index]]))
-                || (contactInformationkeys[index] === "phone"
-                    && !phoneNumberRegex.test(contactInformation[contactInformationkeys[index]])));
-        }).length > 0;
-    }
+	const handleValidatingcontactInformation = () => {
+		const contactInformationkeys = Object.keys(
+			contactInformation as ContactInformation
+		);
+		return (
+			formhandler?.forms[activeStep]?.formFields?.filter((item, index) => {
+				return (
+					!contactInformation[contactInformationkeys[index]] ||
+					(contactInformationkeys[index] === 'email' &&
+						!emailRegex.test(
+							contactInformation[contactInformationkeys[index]]!
+						)) ||
+					(contactInformationkeys[index] === 'phone' &&
+						!phoneNumberRegex.test(
+							contactInformation[contactInformationkeys[index]]!
+						))
+				);
+			}).length > 0
+		);
+	};
 
-    const handleNext = () => {
-        const form = formhandler?.forms[activeStep];
+	const handleNext = () => {
+		const form = formhandler?.forms[activeStep];
 
-        if (activeStep === 0
-            && form.formHasLoaded
-            && handleCheckinglineItem()) {
-            setErrors({
-                ...error,
-                isActive: true,
-                message: 'Please enter a quantity to proceed.'
-            });
-            setOpen(true);
-            return;
-        } else if (activeStep === 1
-            && form.formHasLoaded
-            && handleValidatingcontactInformation()) {
-            setErrors({
-                ...error,
-                isActive: true,
-                message: 'Please enter a value for the required fields in the correct format.'
-            });
+		if (activeStep === 0 && form.formHasLoaded && handleCheckinglineItem()) {
+			setErrors({
+				...error,
+				isActive: true,
+				message: 'Please enter a quantity to proceed.',
+			});
+			setOpen(true);
+			return;
+		} else if (
+			activeStep === 1 &&
+			form.formHasLoaded &&
+			handleValidatingcontactInformation()
+		) {
+			setErrors({
+				...error,
+				isActive: true,
+				message:
+					'Please enter a value for the required fields in the correct format.',
+			});
 
-            setOpen(true);
-            return;
-        } else {
-            setErrors({
-                ...error,
-                isActive: false,
-                message: ''
-            });
-        };
+			setOpen(true);
+			return;
+		} else {
+			setErrors({
+				...error,
+				isActive: false,
+				message: '',
+			});
+		}
 
-        if (activeStep === steps.length - 1) {
-            const filteredLineitems = lineItems.filter((item: LineItem, index: number) => {
-                return item.quantity && item.quantity > 0
-            });
-            //Todo send data to server
-            //reroute user to confirmation page
+		if (activeStep === steps.length - 1) {
+			const filteredLineitems = lineItems.filter(
+				(item: LineItem, index: number) => {
+					return item.quantity && item.quantity > 0;
+				}
+			);
+			//Todo send data to server
+			//reroute user to confirmation page
 
-            const totalCost = filteredLineitems
-                .map((item: LineItem, index: number) => {
-                    const quantity = isNaN(filteredLineitems[index]?.quantity) ? 0 : filteredLineitems[index]?.quantity;
-                    const totalAmount = quantity * item.cost;
-                    return (totalAmount);
-                }).reduce((acc, value) => acc + value)
-                .toFixed(2);
-            addOrder(
-                {
-                    variables:
-                    {
-                        "order":
-                        {
-                            "firstName": contactInformation.firstName,
-                            "lastName": contactInformation.lastName,
-                            "email": contactInformation.email,
-                            "phone": contactInformation.phone,
-                            "totalCost": parseFloat(totalCost),
-                            "lineItems": filteredLineitems,
-                        }
-                    }
-                }
-            ).then((response) => {
-                console.log(response);
-                if (response?.data?.insertOrder) {
-                    navigate(`/storefront/confirmation/${response?.data?.insertOrder}`);
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
-            return;
-        };
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
+			const totalCost = filteredLineitems
+				.map((item: LineItem, index: number) => {
+					const quantity = isNaN(filteredLineitems[index]?.quantity)
+						? 0
+						: filteredLineitems[index]?.quantity;
+					const totalAmount = quantity * item.cost;
+					return totalAmount;
+				})
+				.reduce((acc, value) => acc + value)
+				.toFixed(2);
+			addOrder({
+				variables: {
+					order: {
+						firstName: contactInformation.firstName,
+						lastName: contactInformation.lastName,
+						email: contactInformation.email,
+						phone: contactInformation.phone,
+						totalCost: parseFloat(totalCost),
+						lineItems: filteredLineitems,
+					},
+				},
+			})
+				.then((response) => {
+					console.log(response);
+					if (response?.data?.insertOrder) {
+						navigate(`/storefront/confirmation/${response?.data?.insertOrder}`);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+			return;
+		}
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+	};
 
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-        setErrors({
-            ...error,
-            isActive: false,
-            message: ''
-        });
+	const handleBack = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+		setErrors({
+			...error,
+			isActive: false,
+			message: '',
+		});
+	};
 
-    };
+	const handleReset = () => {
+		setActiveStep(0);
+	};
 
-    const handleReset = () => {
-        setActiveStep(0);
-    };
+	const comps = [
+		React.createElement(Product, {
+			form: 'product',
+			activeStep,
+			lineItems,
+			setLineItem,
+			formhandler,
+			setFormHandler,
+			setFormhasLoadedTrue,
+			setFormhasLoadedFalse,
+			handleSettingFormhandlerFields,
+			handleForcingIsTouchedonallFields,
+			handleNext,
+		}),
+		React.createElement(Contact, {
+			form: 'contact',
+			activeStep,
+			contactInformation,
+			setcontactInformation,
+			formhandler,
+			setFormHandler,
+			setFormhasLoadedTrue,
+			setFormhasLoadedFalse,
+			handleSettingFormhandlerFields,
+		}),
+		React.createElement(Review, {
+			contactInformation,
+			lineItems,
+		}),
+	];
 
-    const comps = [
-        React.createElement(Product,
-            {
-                form: "product",
-                activeStep,
-                lineItems,
-                setLineItem,
-                formhandler,
-                setFormHandler,
-                setFormhasLoadedTrue,
-                setFormhasLoadedFalse,
-                handleSettingFormhandlerFields,
-                handleForcingIsTouchedonallFields,
-                handleNext
-            }),
-        React.createElement(Contact,
-            {
-                form: "contact",
-                activeStep,
-                contactInformation,
-                setcontactInformation,
-                formhandler,
-                setFormHandler,
-                setFormhasLoadedTrue,
-                setFormhasLoadedFalse,
-                handleSettingFormhandlerFields,
-            }),
-        React.createElement(Review,
-            {
-                contactInformation,
-                lineItems,
-            })
-    ];
-
-    return (
-        <StyledLanding>
-            <Box sx={{ width: '100%' }}>
-                <>
-                    <StepperHeader steps={steps} activeStep={activeStep} />
-                    {
-                        (error.isActive)
-                            ?
-                            <ErrorKnockOutModal
-                                open={open}
-                                setOpen={setOpen}
-                                title='Error'
-                                description={error.message}
-                                handleClose={handleClose}
-                            />
-                            : null
-                    }
-                    {
-                        comps.map((item, index) => {
-                            if (index === activeStep) {
-                                return (
-                                    <StyledComponents key={index}>
-                                        {item}
-                                    </StyledComponents>
-                                )
-                            }
-                        })
-                    }
-                    <StepperFooter
-                        steps={steps}
-                        activeStep={activeStep}
-                        handleBack={handleBack}
-                        handleNext={handleNext}
-                    />
-                </>
-            </Box>
-        </StyledLanding >
-    );
-}
+	return (
+		<StyledLanding>
+			<Box sx={{ width: '100%' }}>
+				<>
+					<StepperHeader steps={steps} activeStep={activeStep} />
+					{error.isActive ? (
+						<ErrorKnockOutModal
+							open={open}
+							setOpen={setOpen}
+							title='Error'
+							description={error.message}
+							handleClose={handleClose}
+						/>
+					) : null}
+					{comps.map((item, index) => {
+						if (index === activeStep) {
+							return <StyledComponents key={index}>{item}</StyledComponents>;
+						}
+					})}
+					<StepperFooter
+						steps={steps}
+						activeStep={activeStep}
+						handleBack={handleBack}
+						handleNext={handleNext}
+					/>
+				</>
+			</Box>
+		</StyledLanding>
+	);
+};
 
 export default LemonadeStand;
