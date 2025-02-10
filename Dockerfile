2ea281b08e39c7b06e32aedf2686975a885a7e30
Thomas <thomasrnunez@gmail.com>
2025-02-10 11:25:15 -0500

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:22.13.1 AS builder
+FROM node:18 AS builder
 
 WORKDIR /app
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import reactPlugin from '@vitejs/plugin-react';
-import macrosPlugin from "vite-plugin-babel-macros";
+import macrosPlugin from 'vite-plugin-babel-macros';
 import md from 'vite-plugin-react-md';
-import { Mode, plugin } from 'vite-plugin-markdown'
-const path = require("path");
+import { Mode, plugin } from 'vite-plugin-markdown';
+const path = require('path');
 
 export default defineConfig({
-  plugins: [
-    macrosPlugin(),
-    reactPlugin(),
-    plugin(),
-  ],
+	plugins: [macrosPlugin(), reactPlugin(), plugin()],
+	build: {
+		rollupOptions: {
+			onwarn(warning, warn) {
+				if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+					return;
+				}
+				warn(warning);
+			},
+		},
+	},
 });
