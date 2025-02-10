1ccd3e73e8230024bde2de14fb5dc351e095f201
Thomas <thomasrnunez@gmail.com>
2025-02-10 11:47:05 -0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import macrosPlugin from 'vite-plugin-babel-macros';
 import md from 'vite-plugin-react-md';
 import { Mode, plugin } from 'vite-plugin-markdown';
 const path = require('path');
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-
 export default defineConfig({
-	plugins: [macrosPlugin(), reactPlugin(), plugin(), nodeResolve(), commonjs()],
+	plugins: [macrosPlugin(), reactPlugin(), plugin()],
 	build: {
 		rollupOptions: {
 			onwarn(warning, warn) {
