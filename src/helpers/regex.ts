6712669cebba62ca9637f23a98da33fe85ee5ad4
Thomas <thomasrnunez@gmail.com>
2025-03-23 12:31:05 -0400

    wip

---
+++ src/helpers/regex.ts
@@ -0,0 +1,6 @@
+export const atleastOneuppercase = '/(?=.*[A-Z])/';
+export const atleastOnelowercase = '/(?=.*[a-z])/';
+export const atleastOneNumber = '/\\d/';
+export const allowedCharacters = '/[A-Za-zd@$!%*?&]/';
+export const minimumNumbercharacters = '/{8,}/';
+export const allowedSpecialCharacters = '/(?=.*[@$!%*?&])/';
